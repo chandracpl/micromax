d8db6555f02622329445007ce70d64f206aa2d4c
Your Name <you@example.com>
2019-08-01 14:31:24 +0000

    micromax

---
+++ src/main/java/com/micromax/App.java
@@ -0,0 +1,13 @@
+package com.micromax;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
